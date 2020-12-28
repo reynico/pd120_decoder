f6b21b95a499fe707f7fbabf05b59d7c7fc36a71
Nico Rey <rey.nico@gmail.com>
2020-12-28 20:34:59 -0300

    decorate decoder with a header detector. ability to decode more than one pass in the same wav file, english translations

--- pd120_decoder/demod.py
+++ pd120_decoder/demod.py
@@ -4,28 +4,46 @@ from scipy.io import wavfile
 import numpy as np
 import scipy.signal
 
-import matplotlib.pyplot as plt
-import pylab
-import math
-from utils import escribir_pixel, filtrar
+# import matplotlib.pyplot as plt
+# import pylab
+from utils import write_px, filt
 from PIL import Image
 import sys
 
-''' constantes '''
+''' constants '''
 PORCH_TIME = 0.00208
 SYNC_TIME = 0.02
 LINE_COMP_TIME = 0.1216
 
-# fs, data = wavfile.read('./iss.wav')
+# Carrier detection on header frequency
+# written as a raw value from the conversion.
+# The threshold helps detection on noisy signal.
+freq = 2270
+threshold = 100
+
+# How many samples do we need to take care of
+# to detect the header.
+items = 700
+
+# PD120 transmission duration expressed as
+# a raw value from the wav file conversion.
+pass_len = 1425000
+
+# How many samples do we need to wait until a
+# new pass.
+wait_time = pass_len
+
+# If the same audio file has more than one
+# transmission, then store the header index
+# here.
+header_end = []
+idx = 0
+
 fs, data = wavfile.read(sys.argv[1])
 
 img = Image.new('YCbCr', (640, 496), "white")
-#t = np.arange(len(data))/fs
 
-
-def crear_hilbert(atten, delta):
-    ''' crea el filtro de hilbert enventanado por kaiser '''
-
+def create_hilbert(atten, delta):
     if atten < 21:
         beta = 0
     elif atten > 21 and atten < 50:
@@ -45,93 +63,84 @@ def crear_hilbert(atten, delta):
         else:
             filter.append(0)
     hilbert = filter * window
+    return hilbert
 
-    return(hilbert)
 
-
-def crear_analitica(datos, filtro):
-    '''devuelve la senal analitica de la forma x + y*j'''
-
+def create_analytica(datos, filtro):
     zeros = np.zeros((len(filtro)-1)//2)
-    datareal = np.concatenate([zeros, datos, zeros])
-    datacompleja = np.convolve(datos, filtro)*1j
-    senal = datareal + datacompleja
+    realdata = np.concatenate([zeros, datos, zeros])
+    complexdata = np.convolve(datos, filtro)*1j
+    return realdata + complexdata
 
-    return(senal)
-
-
-def boundary(value):
-    '''checkea los valores maximos y minimos de la frecuencia instantanea'''
-
-    value = min(valor, 2300)
-    value = max(1500, valor)
-
-    return(value)
-
+def boundary(val):
+    value = min(val, 2300)
+    value = max(1500, val)
+    return value
 
 def hpf(data, fs):
     firw = scipy.signal.firwin(101, cutoff=800, fs=fs, pass_zero=False)
     return scipy.signal.lfilter(firw, [1.0], data)
 
+def decode(start, samples_list):
+    samples = 0
+    cont_line = -1
+    i = 0
+    while i < len(samples_list):
+        if 900 <= samples_list[i] <= 1300:
+            samples += 1
+        if samples > int((SYNC_TIME-0.002)*fs):
+            cont_line += 2
+            samples = 0
+            i = i-int((SYNC_TIME-0.002)*fs)+int((SYNC_TIME+PORCH_TIME)*fs)
+            gap = 1200 - np.mean(samples_list[i-int((SYNC_TIME+PORCH_TIME)*fs): i-int(PORCH_TIME*fs)])
 
-signal = crear_analitica(hpf(data, fs), crear_hilbert(
-    40, np.pi/1200))  # frecuencia normalizada
+            try:
+                y_resampled = scipy.signal.resample(samples_list[i:i+int(LINE_COMP_TIME*fs)], 640)
+                for col, val in enumerate(y_resampled):
+                    write_px(img, col, cont_line,"lum", boundary(val+gap))
 
-# xunwrap deja a la fase de forma lineal en vez de rampa
+                cr_resampled = scipy.signal.resample(samples_list[i+int(LINE_COMP_TIME*fs):i+int(LINE_COMP_TIME*2*fs)], 640)
+                for col, val in enumerate(cr_resampled):
+                    write_px(img, col, cont_line,"cr", boundary(val+gap))
+
+                cb_resampled = scipy.signal.resample(samples_list[i+int(LINE_COMP_TIME*2*fs):i+int(LINE_COMP_TIME*3*fs)], 640)
+                for col, val in enumerate(cb_resampled):
+                    write_px(img, col, cont_line,"cb", boundary(val+gap))
+
+                ny_resampled = scipy.signal.resample(samples_list[i+int(LINE_COMP_TIME*3*fs):i+int(LINE_COMP_TIME*4*fs)], 640)
+                for col, val in enumerate(ny_resampled):
+                    write_px(img, col, cont_line,"nxt_lum", boundary(val+gap))
+            except:
+                break
+            i += int(LINE_COMP_TIME*2*fs)
+        i += 1
+    imgrgb = img.convert("RGB")
+    imgrgb.save("%s-%s.png" % (sys.argv[1], start), "PNG")
+
+signal = create_analytica(hpf(data, fs), create_hilbert(40, np.pi/1200))
+
+# xunwrap converts ramp-phase to linear
 inst_ph = np.unwrap(np.angle(signal))
-# diff toma el valor de x(n+1) y lo resta con x(n)
+
 inst_fr = (np.diff(inst_ph) / (2.0*np.pi) * fs)
+inst_fr = list(filt(inst_fr, 0.2, 0.2, 40))
 
-# toma senal, frec corte, banda de trans, y caida en dB
-inst_fr = list(filtrar(inst_fr, 0.2, 0.2, 40))
+while idx < len(inst_fr):
+    if all((freq - threshold)<x<(freq + threshold) for x in inst_fr[idx:idx+items:1]):
+        header_end.append(idx)
+        print("found carrier at sample %s" % idx)
+        idx += wait_time
+    else:
+        idx += 1
 
-muestras = 0
-cont_linea = -1
-i = 0
+# fig = plt.figure()
+# ax0 = plt.subplot(211)
+# ax0.plot(range(len(inst_fr)), inst_fr)
+# #ax1 = plt.subplot(212)
+# #ax1.plot(t[i+5600*3+1:i+5400*4+1],inst_fr[i+5600*3:i+5400*4])
+# pylab.show()
 
-while i < len(inst_fr):
 
-    if 900 <= inst_fr[i] <= 1300:
-        muestras += 1  # contador de muestras, si muchas se encuentran en el rango era cambio de linea
-
-    if muestras > int((SYNC_TIME-0.002)*fs):
-        cont_linea += 2  # casi seguro indico la siguiente
-        muestras = 0  # resetear muestras para la proxima iteracion
-        # encajar i para comenzar justo en luminancia
-        i = i-int((SYNC_TIME-0.002)*fs)+int((SYNC_TIME+PORCH_TIME)*fs)
-        desfase = 1200 - \
-            np.mean(inst_fr[i-int((SYNC_TIME+PORCH_TIME)*fs): i-int(PORCH_TIME*fs)])
-
-        try:
-            y_resampleados = scipy.signal.resample(
-                inst_fr[i:i+int(LINE_COMP_TIME*fs)], 640)
-            for columna, valor in enumerate(y_resampleados):
-                escribir_pixel(img, columna, cont_linea,
-                               "lum", boundary(valor+desfase))
-
-            cr_resampleados = scipy.signal.resample(
-                inst_fr[i+int(LINE_COMP_TIME*fs):i+int(LINE_COMP_TIME*2*fs)], 640)
-            for columna, valor in enumerate(cr_resampleados):
-                escribir_pixel(img, columna, cont_linea,
-                               "cr", boundary(valor+desfase))
-
-            cb_resampleados = scipy.signal.resample(
-                inst_fr[i+int(LINE_COMP_TIME*2*fs):i+int(LINE_COMP_TIME*3*fs)], 640)
-            for columna, valor in enumerate(cb_resampleados):
-                escribir_pixel(img, columna, cont_linea,
-                               "cb", boundary(valor+desfase))
-
-            ny_resampleados = scipy.signal.resample(
-                inst_fr[i+int(LINE_COMP_TIME*3*fs):i+int(LINE_COMP_TIME*4*fs)], 640)
-            for columna, valor in enumerate(ny_resampleados):
-                escribir_pixel(img, columna, cont_linea,
-                               "nxt_lum", boundary(valor+desfase))
-        except:
-            break
-
-        i += int(LINE_COMP_TIME*2*fs)
-
-    i += 1
-
-imgrgb = img.convert("RGB")
-imgrgb.save("%s.png" % sys.argv[1], "PNG")
+for padding in header_end:
+    print("start: %s \t end: %s" % (padding, padding+pass_len))
+    decode(padding,inst_fr[padding:padding+pass_len])
