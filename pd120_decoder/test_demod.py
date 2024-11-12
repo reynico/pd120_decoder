1521fc7d592c67c03bd9e97b7cdd4792609cc5d8
Nicolas Rey <reynico@gmail.com>
2024-11-11 21:19:32 -0300

    use absolute paths for testing files

--- pd120_decoder/test_demod.py
+++ pd120_decoder/test_demod.py
@@ -146,8 +146,9 @@ def compare_images(img1, img2, tolerance=10):
 
 
 def test_process_audio(temp_output_dir):
-    input_wav = "./examples/iss-20201225-002100-short.wav"
-    reference_image_path = "./examples/iss-20201225-002100-short.wav-7766.png"
+    root_dir = Path(__file__).resolve().parent.parent
+    input_wav = root_dir / "examples/iss-20201225-002100-short.wav"
+    reference_image_path = root_dir / "examples/iss-20201225-002100-short.wav-7766.png"
 
     assert Path(input_wav).exists(), f"Input WAV file not found: {input_wav}"
     assert Path(reference_image_path).exists(), f"Reference image not found: {reference_image_path}"
